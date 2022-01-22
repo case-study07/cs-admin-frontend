c8cc50c8c0baa4fe1675c8327ebec051cec5587d
yusuke <38mtyuusuuke@gmail.com>
2022-01-22 12:56:44 +0900

    tailwindcss 環境構築

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ["tailwindcss", "postcss-nesting", "autoprefixer"],
+};
+
+// 出力用
+// module.exports = {
+//     plugins: [
+//         require("tailwindcss"), require("autoprefixer")
+//     ],
+// };
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        "header-color": "#F26969",
+        "footer-color": "#FFB9B9",
+        "main-color": "#E8E7E3",
+        "link-color": "#4A6EEF",
+        "category-color": "#E5E8EF",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
